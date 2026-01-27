8bc973233cac1b136a3a5b6e2f0cd7434e68bd2e
michael <michaelegharevba490@gmail.com>
2026-01-27 18:12:11 +0000

    Added switch statement

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-function showTraining() {
-
+function setTraining() {
+    const day = document.querySelector("select").value;
+    let morningTraining;
+    let eveningTraining;
+    let restDay = false;
+    switch (day) {
+        case "Monday":
+            morningTraining = "Suicide runs";
+            eveningTraining = "Bag work";
+            break;
+        case "Tuesday":
+            morningTraining = "Strength training: Upper body";
+            eveningTraining = "15 rounds sparring";
+            break;
+        case "Wednesday":
+            morningTraining = "7.5km run and calisthenics";
+            eveningTraining = "Jump rope and padwork";
+            break;
+        case "Thursday":
+            morningTraining = "None";
+            eveningTraining = "None";
+            restDay = true;
+            break;
+        case "Friday":
+            morningTraining = "Jump rope and footwork drills";
+            eveningTraining = "Circuits and shadowboxing";
+            break;
+        case "Saturday":
+            morningTraining = "Hill sprints and sparring";
+            eveningTraining = "Strength training: Lower body";
+            break;
+        case "Sunday":
+            morningTraining = "None";
+            eveningTraining = "None";
+            restDay = true;
+    }
 }
